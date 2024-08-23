7525af6db46fed42c33465325bb5a063b78a2435
saima9 <saimaparween281@gmail.com>
2024-08-24 04:41:19 +0530

    completed

--- global-news-wave/src/Components/Card.js
+++ global-news-wave/src/Components/Card.js
@@ -2,27 +2,28 @@ import React from "react";
 
 const Card=({data})=>{
     console.log(data)
-    const readMore = (url) =>{
-        window.open(url)
-    }
-;
-
+    
 
     return (
         <div className="cardContainer">
 
             {
                 data.map((item,index)=>{
+                    if(!item.urlToImage){
+            return null
+        }else{
+                
                     return (
                         <div className="card" key={index}>
                         <img src={item.urlToImage} alt="article_image" />
                         <div className="content"> 
-                        <a href={item.url} className="title">{item.title}</a>
+                        <a    href={item.url} className="title">{item.title}</a>
                         <p>{item.description}</p>
-                        <button onClick={readMore(item.url)}>Read More</button>
+                        <button onClick={()=> window.open(item.url)}>Read More</button>
                         </div>
                         </div>
                     )
+        }
                 })
             }
         </div>
--- global-news-wave/src/Components/Newsapp.js
+++ global-news-wave/src/Components/Newsapp.js
@@ -1,56 +1,74 @@
-import React, { useState } from 'react';
-import Card from './Card'
-const Newsapp =()=>{
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+const Newsapp = () => {
+  const API_KEY = "709942faa7c34215a5e4f5c1ebf3c13d";
+  const [search, setSearch] = useState("india");
+  const [newsData, setNewsData] = useState(null);
 
-
-const API_KEY= "709942faa7c34215a5e4f5c1ebf3c13d";
-const [search, setSearch]=useState('india');
-const [newsData, setNewsData]=useState([])
-
-const getData= async()=>{
-    const response= await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`)
+  const getData = async () => {
+    const response = await fetch(
+      `https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`
+    );
     const jsonData = await response.json();
     console.log(jsonData.articles);
-    setNewsData(jsonData.articles)
-}
-const handelInput= (e)=>{
-setSearch(e.target.value);
 
+    setNewsData(jsonData.articles);
+    setSearch("")
+  };
 
-}
+  useEffect(() => {
+    getData();
+  }, []);
 
-    return (
-  <div>   
-    <nav>
-         <div>
-            <h1>Trendy News</h1>
+  const handelInput = (e) => {
+    setSearch(e.target.value);
+  };
+  const inputData = (event) => {
+    setSearch(event.target.value);
+  };
+
+  return (
+    <div>
+      <nav>
+        <div>
+          <h1>Trendy News</h1>
         </div>
-        <ul>
-            <a>All News</a>
-            <a>Trending</a>
+        <ul style={{ display: "flex", gap: "11px" }}>
+          <a style={{ fontWeight: 600, fontSize: "17px" }}>All News</a>
+          <a style={{ fontWeight: 600, fontSize: "17px" }}>Trending</a>
         </ul>
-        <div className='searchBar'> 
-        <input type='text' placeholder='Search News' onChange={handelInput}/>
-        <button onClick={getData}> Search</button>
+        <div className="searchBar">
+          <input
+            type="text"
+            placeholder="Search News"
+            value={search}
+            onChange={handelInput}
+          />
+          <button onClick={getData}> Search</button>
         </div>
-    </nav>
-    <div>
-        <p className='head'> Stay Update with TrendyNews</p>
+      </nav>
+      <div>
+        <p className="head"> Stay Update with TrendyNews</p>
+      </div>
+      <div className="categoryBtn">
+        <button onClick={inputData} value="sports">
+          Sports
+        </button>
+        <button onClick={inputData} value="politics">
+          Politics
+        </button>
+        <button onClick={inputData} value="entertainment">
+          Entertainment
+        </button>
+        <button onClick={inputData} value="health">
+          Health
+        </button>
+        <button onClick={inputData} value="fitness">
+          Fitness
+        </button>
+      </div>
+      <div>{newsData ? <Card data={newsData} /> : null}</div>
     </div>
-    <div className='categoryBtn'>
-        <button>Sports</button>
-        <button>Politics</button>
-        <button>Entertainment</button>
-        <button>Health</button>
-        <button>Fitness</button>
-    </div>
-    <div>
-
-    </div>
-<Card data={newsData} />
-        
- </div>
-        
-    )
-}
+  );
+};
 export default Newsapp;
